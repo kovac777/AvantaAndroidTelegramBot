d86b245f98d29d602c3f872dcadcec1d9d3fefdd
maksim <mvinnik@avanta.co>
2020-07-15 14:43:37 +0300

    changed arguments logic

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,67 +9,25 @@ const val PARSE_MODE = "markdown"
 const val ANDROID_ROLLOUT_BOT_TOKEN = "983501720:AAE-AH2qamz4GZaEzqelz0i0QZnavvzMDS8"
 
 
+// args[0] - Project name
+// args[1] - Build version
+// args[2] - Description
 fun main(args: Array<String>) = runBlocking<Unit> {
-    // args[0] - bot type
-    when (args[0]) {
-        "rollout_build_meetville" -> {
-            // args[1] - android build version
-            // args[2] - android build description
-            val version = args[1]
-            var description = ""
-            try {
-                description = args[2]
-                sendRolloutMeetvilleNotification(version, description)
-            } catch (ex: Throwable) {
-                sendRolloutMeetvilleNotification(version, description)
-            }
-            exitProcess(0)
-        }
-        "rollout_build_good_book" -> {
-            // args[1] - android build version
-            // args[2] - android build description
-            val version = args[1]
-            var description = ""
-            try {
-                description = args[2]
-                sendRolloutGoodBookNotification(version, description)
-            } catch (ex: Throwable) {
-                sendRolloutGoodBookNotification(version, description)
-            }
-            exitProcess(0)
-        }
-        "rollout_build_wink" -> {
-            // args[1] - android build version
-            // args[2] - android build description
-            val version = args[1]
-            var description = ""
-            try {
-                description = args[2]
-                sendRolloutWinkNotification(version, description)
-            } catch (ex: Throwable) {
-                sendRolloutWinkNotification(version, description)
-            }
-            exitProcess(0)
-        }
-        else -> throw IllegalStateException("Can't define bot type!")
+    val projectName = args[0]
+    val buildVersion = args[1]
+    val description = try {
+        args[2]
+    } catch (e: Exception) {
+        ""
     }
+
+    sendRolloutNotification(projectName, buildVersion, description)
+    exitProcess(0)
 }
 
-suspend fun sendRolloutMeetvilleNotification(version: String, description: String) {
+suspend fun sendRolloutNotification(projectName: String, buildVersion: String, description: String) {
     val chatId = -346458703
-    val text = "Meetville билд залит! *Версия $version*\n_${description}_"
-    getBotService().sendRolloutNotification(chatId.toString(), text, PARSE_MODE)
-}
-
-suspend fun sendRolloutGoodBookNotification(version: String, description: String) {
-    val chatId = -346458703
-    val text = "GoodBook билд залит! *Версия $version*\n_${description}_"
-    getBotService().sendRolloutNotification(chatId.toString(), text, PARSE_MODE)
-}
-
-suspend fun sendRolloutWinkNotification(version: String, description: String) {
-    val chatId = -346458703
-    val text = "Wink билд залит! *Версия $version*\n_${description}_"
+    val text = "$projectName билд залит! *Версия $buildVersion*\n_${description}_"
     getBotService().sendRolloutNotification(chatId.toString(), text, PARSE_MODE)
 }
 
